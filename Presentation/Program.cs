eecd87d946aa749b80dd8bfb97f9f0e0ecd17a8a
Tonlito <Antoniow4@hotmail.com>
2025-06-07 13:37:00 +0200

    Added KeyVault

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,9 +1,12 @@
 using Azure.Communication.Email;
+using Azure.Identity;
 using Business.Interfaces;
 using Business.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var keyVaultUrl = new Uri("https://keyvault-mvp-student.vault.azure.net/");
+builder.Configuration.AddAzureKeyVault(keyVaultUrl, new DefaultAzureCredential());
+Console.WriteLine("ACS Conn: " + builder.Configuration["ACS:ConnectionString"]);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
